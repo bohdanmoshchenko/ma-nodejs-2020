9b5e2f378aa5a3ee299eed0b017f483d9df1219c
Bogdan Moshchenko <bohdan.moshchenko@gmail.com>
2019-10-25 21:37:54 +0300

    add 3 task

---
+++ js-check/3.js
@@ -0,0 +1,9 @@
+let text = 'Hello World!';
+for (let index = 0; index < text.length; index++) {
+    char = text[index];
+    if (char === 'o') {
+        console.log(index + 1);
+    }
+}
+
+console.log(text.replace(/l/g, ''));
