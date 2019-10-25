53969a901185889437f4e7309c312448e0fd8460
Bogdan Moshchenko <bohdan.moshchenko@gmail.com>
2019-10-25 21:38:31 +0300

    add 7 task

---
+++ js-check/7.js
@@ -0,0 +1,18 @@
+const vegetables = ['potato', 'tomato', 'cucumber'];
+const fruits = ['apple', 'pineapple', 'banana'];
+
+const word = 'cucumber';
+if (vegetables.includes(word)) {
+    console.log(Object.keys({vegetables}));
+} else if (fruits.includes(word)) {
+    console.log(Object.keys({fruits}));
+}
+
+switch (true) {
+    case vegetables.includes(word):
+        console.log(Object.keys({vegetables}));
+        break;
+    case fruits.includes(word):
+        console.log(Object.keys({fruits}));
+        break;
+}
